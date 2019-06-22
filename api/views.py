0817d8200fe4a28aebf16c2af89b3b3e8893fcaa
PonesRyang <563103310@qq.com>
2019-06-22 20:24:13 +0800

    添加接口应用

--- api/views.py
+++ api/views.py
@@ -8,10 +8,11 @@ from django.db.transaction import atomic
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
-from rest_framework.generics import ListAPIView,RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from api.models import Users, Article, Comment, Complain, OrderComplain, Orders, Roles, OrderType, District
-from api.serializers import ArticleSerializer, OrderDetailSerializer,StarStaffSerializer, OrdersTypeSerializer, DistrictSimpleSerializer, \
-    DistrictDetailSerializer
+from api.serializers import ArticleSerializer, OrderDetailSerializer, StarStaffSerializer, OrdersTypeSerializer, \
+	DistrictSimpleSerializer, \
+	DistrictDetailSerializer
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
@@ -20,72 +21,72 @@ from api.forms import UserInfoForm
 
 
 class DistrictViewSet(ListAPIView):
-    queryset = District.objects.all()
-    serializer_class = DistrictSimpleSerializer
+	queryset = District.objects.all()
+	serializer_class = DistrictSimpleSerializer
 
 
 @api_view(['POST'])
 def SubmitList(request):
-    order_addr = request.data.get('order_addr')
-    order_plantime = request.data.get('order_plantime')
-    district = request.data.get('district')
-    district1 = District.objects.get(name=district)
-    user = request.session.get('user')
-    if order_addr and order_plantime:
-        current_time = timezone.now()
-        order =Orders()
-        order.order_addr = order_addr
-        order.order_plantime = order_plantime
-        order.order_createtime = current_time
-        order.district = district1
-        order.u = user
-        order.order_status = 2
-        order.order_number = uuid.uuid4().hex
-        order.save()
-        data={
-            'code':200, 'message':'订单已生成'
-        }
-    else:
-        data={
-            'code':300, 'message':'订单地址不能为空'
-        }
-    return Response(data)
+	order_addr = request.data.get('order_addr')
+	order_plantime = request.data.get('order_plantime')
+	district = request.data.get('district')
+	district1 = District.objects.get(name=district)
+	user = request.session.get('user')
+	if order_addr and order_plantime:
+		current_time = timezone.now()
+		order = Orders()
+		order.order_addr = order_addr
+		order.order_plantime = order_plantime
+		order.order_createtime = current_time
+		order.district = district1
+		order.u = user
+		order.order_status = 2
+		order.order_number = uuid.uuid4().hex
+		order.save()
+		data = {
+			'code': 200, 'message': '订单已生成'
+		}
+	else:
+		data = {
+			'code': 300, 'message': '订单地址不能为空'
+		}
+	return Response(data)
 
 
 class StarStaffView(ListAPIView):
-    """查看星级家政人员"""
-    queryset = Users.objects.all()
-    serializer_class = StarStaffSerializer
-    filter_backends = (DjangoFilterBackend, OrderingFilter)
-    filter_class = StarStaffFilterSet
+	"""查看星级家政人员"""
+	queryset = Users.objects.all()
+	serializer_class = StarStaffSerializer
+	filter_backends = (DjangoFilterBackend, OrderingFilter)
+	filter_class = StarStaffFilterSet
 
-    def get_queryset(self):
-        role = Roles.objects.filter(r_code=1)
-        queryset = Users.objects.filter(role=role)
-        return queryset
+	def get_queryset(self):
+		role = Roles.objects.filter(r_code=1)
+		queryset = Users.objects.filter(role=role)
+		return queryset
 
 
 class OrdersTypeViewSet(ModelViewSet):
-    queryset = OrderType.objects.all()
-    serializer_class = OrdersTypeSerializer
-    pagination_class = None
+	queryset = OrderType.objects.all()
+	serializer_class = OrdersTypeSerializer
+	pagination_class = None
 
 
 # 获取所有省级行政单位 - GET /api/districts/
 class ProvinceView(ListAPIView):
-    """查看各省的id和名称"""
-    # only defer 优化SQL查询
-    queryset = District.objects.filter(parent__isnull=True).only('distid',)
-    # queryset = District.objects.filter(parent__isnull=True).defer('ishot','intro')
-    serializer_class = DistrictSimpleSerializer
-    pagination_class = None
+	"""查看各省的id和名称"""
+	# only defer 优化SQL查询
+	queryset = District.objects.filter(parent__isnull=True).only('distid', )
+	# queryset = District.objects.filter(parent__isnull=True).defer('ishot','intro')
+	serializer_class = DistrictSimpleSerializer
+	pagination_class = None
 
 
 # 获取指定行政单位详情及其管辖的行政单位 - GET /api/districts/{行政单位编号}
 class DistrictView(RetrieveAPIView):
-    """查看各省的id,名称,介绍及其下各市区的id和名称"""
-    queryset = District.objects.all()
-    serializer_class = DistrictDetailSerializer
+	"""查看各省的id,名称,介绍及其下各市区的id和名称"""
+	queryset = District.objects.all()
+	serializer_class = DistrictDetailSerializer
 
 
 @api_view(['POST'])
@@ -192,37 +193,39 @@ def complain(request):
 	except:
 		data = {'code': 300, 'mes': '投诉失败,请稍后再试'}
 	return JsonResponse(data)
+
+
 def to_md5_hex(origin_str):
-    """生成MD5摘要"""
-    password = md5(origin_str.encode('utf-8')).hexdigest()
-    return password
+	"""生成MD5摘要"""
+	password = md5(origin_str.encode('utf-8')).hexdigest()
+	return password
 
 
 @api_view(['POST'])
 def user_info(request):
-    """完善个人信息（增加用户名和密码）"""
-    user = request.session.get('user')
-    # user = Users.objects.get(u_id=1)
-    form = UserInfoForm(request.POST)
-    if form.is_valid():
-        user.u_nickname = form.cleaned_data['nickname']
-        user.u_password = to_md5_hex(form.cleaned_data['password'])
-        user.save()
-        data = {'code': 200, 'message': '操作成功'}
-        return JsonResponse(data=data)
+	"""完善个人信息（增加用户名和密码）"""
+	user = request.session.get('user')
+	# user = Users.objects.get(u_id=1)
+	form = UserInfoForm(request.POST)
+	if form.is_valid():
+		user.u_nickname = form.cleaned_data['nickname']
+		user.u_password = to_md5_hex(form.cleaned_data['password'])
+		user.save()
+		data = {'code': 200, 'message': '操作成功'}
+		return JsonResponse(data=data)
 
 
 @api_view(['POST'])
 def order_finish_or_cancel(request):
-    """完成订单或者取消订单"""
-    order_number = request.data.get('order_number')
-    status = request.data.get('order_status')
-    order = Orders.objects.filter(order_number=order_number).first()
-    if status == 0:
-        order.order_status = 0
-        order.save()
-    else:
-        order.order_status = 1
-        order.save()
-    data = {'code': 200, 'message': '操作成功'}
-    return JsonResponse(data=data)
+	"""完成订单或者取消订单"""
+	order_number = request.data.get('order_number')
+	status = request.data.get('order_status')
+	order = Orders.objects.filter(order_number=order_number).first()
+	if status == 0:
+		order.order_status = 0
+		order.save()
+	else:
+		order.order_status = 1
+		order.save()
+	data = {'code': 200, 'message': '操作成功'}
+	return JsonResponse(data=data)
