50baaafdaff494e5421e942d0b16ddd97c63bfb1
PonesRyang <563103310@qq.com>
2019-06-24 14:32:17 +0800

    增加账号认证

--- api/middlewares.py
+++ api/middlewares.py
@@ -1,19 +1,20 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render, redirect
 
 
 def login_authentication_middleware(func):
-    def wrapper(request, *args, **kwargs):
-        try:
-            un_authentication_list = ['/login/','/logout']
-            if request.session.get('user'):
-                resp = func(request, *args, **kwargs)
-            else:
-                resp = redirect('/login/')
-            if request.path in un_authentication_list:
-                resp = func(request, *args, **kwargs)
-        except Exception:
-            # 记录异常日志
-            resp = redirect('/login/')
-        return resp
-    return wrapper
+	def wrapper(request, *args, **kwargs):
+		try:
+			un_authentication_list = ['/api/login/', '/api/captcha/', '/api/mobile/<str:tel>/']
+			if request.session.get('user'):
+				resp = func(request, *args, **kwargs)
+			else:
+				return JsonResponse(data={'code': 300, 'mes': '请登陆后再试'})
+			if request.path in un_authentication_list:
+				resp = func(request, *args, **kwargs)
+		except Exception:
+			# 记录异常日志
+			resp = redirect('/login/')
+		return resp
+
+	return wrapper
