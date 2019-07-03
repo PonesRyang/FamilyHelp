c8b69e6a8d208a1236aadf0a8d26ab20a0f4292c
邱鲜 <861790597@qq.com>
2019-07-03 19:38:54 +0800

    第一次提交

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,12 @@ from api import views
 
 urlpatterns = [
     path('personal_information/', views.change_information),
-    path('logout/', views.logout)
+    path('logout/', views.logout),
+    path('mobile/<str:tel>/', views.send_mobile_code),
+    # path('register/',views.register),
+    path('login/',views.login),
+    path('captcha/',views.get_captcha),
+
 ]
 
 router = DefaultRouter()
--- api/views.py
+++ api/views.py
@@ -1,11 +1,77 @@
+from django.contrib.auth.models import User
+from django.core.cache import caches
 from django.db.transaction import atomic
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from rest_framework.decorators import api_view
 
+from api.forms_helper import LoginForm
 from api.models import Users
+from captcha import Captcha
+from utils import random_captcha_text, random_mobile_code, send_code_by_sms
+
+
+def get_captcha(request):
+	""" 验证码 """
+	captcha_text = random_captcha_text()
+	request.session['captcha'] = captcha_text
+	image_data = Captcha.instance().generate(captcha_text)
+	return HttpResponse(image_data, content_type='image/png')
+
+def send_mobile_code(request, tel):
+    """发送短信验证码"""
+    code = random_mobile_code()
+    if tel in caches['mobile']:
+        data = {'code': 403, 'message': '请不要在120秒内重复发送'}
+    else:
+        result = send_code_by_sms(tel, code)
+        if result['error'] == 0:
+            # 将手机验证码保存到缓存中
+            caches['mobile'].set(tel, code, timeout=120)
+            data = {'code': 200, 'message': '短信验证码已发送'}
+        else:
+            data = {'code': 404, 'message': '短信验证码服务暂时无法使用'}
+    return JsonResponse(data)
+
+
+
+# @api_view(['GET','POST'])
+# def register(request):
+# 	if request.method=='GET':
+# 		pass
+# 	if request.method=='POST':
+# 		pass
+
+
+
+@api_view(['GET','POST'])
+def login(request):
+	if request.method=='GET':
+		return render(request, 'login.html')
+	if request.method=='POST':
+		form  = LoginForm(request.POST)
+		if form.is_valid():
+			captcha_from_user = form.cleaned_data['captcha']
+			captcha_from_sess = request.session.get('captcha', '')
+			if captcha_from_sess.lower() != captcha_from_user.lower():
+				hint = '请输入正确的图形验证码'
+			else:
+				tel = form.cleaned_data['tel']
+				# user = User.objects.filter(tel=tel).first()
+				# if tel:
+				# request.session['user'] = user
+				return HttpResponse('跳转首页成功')
+				# else:
+				# 	hint = '用户名或密码错误'
+		else:
+			hint = '请输入有效的登录信息'
+	return render(request, 'login.html', {'hint': hint})
+
+
+
+
 
 
 @api_view(['PUT'])
