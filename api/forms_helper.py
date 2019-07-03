653c9d5f7cac5ed00d534ae996a165e80dd95de8
邱鲜 <861790597@qq.com>
2019-07-03 19:39:39 +0800

    第一次提交

---
+++ api/forms_helper.py
@@ -0,0 +1,20 @@
+import re
+
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+
+
+class LoginForm(forms.Form):
+    tel = forms.CharField(min_length=11, max_length=11)
+    captcha = forms.CharField(min_length=4, max_length=4)
+    # phoneCode = forms.CharField(min_length=6, max_length=6)
+
+
+    def clean_tel(self):
+        tel = self.cleaned_data['tel']
+        pattern = re.compile(r'\d{11}')
+        if not pattern.fullmatch(tel):
+            raise ValidationError('请输入有效手机号')
+        return tel
